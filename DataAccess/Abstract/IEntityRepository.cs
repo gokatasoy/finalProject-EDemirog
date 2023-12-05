5815c63fecf67e7da02d38ffd3b67c1c2648f646
Gökberk ATASOY <gokatasoycs@gmail.com>
2023-12-05 13:23:31 +0300

    Implement entity framework and make the first connections with the mssql database.

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Entities.Abstract;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,10 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    //class : referans tip olabilir demek
+    //"T" bir referans tip olmalı ve ya IEntity olabilir ya da IEntity'den implemente olan bir şey olabilir. IEntity referanslı olan classlar ise Category, Customer, Product.
+    //new() : new'leneiblir olmalı. IEntity interface olduğu için new'lenemez. Bundan dolayı IEntity implemente edilmiş class'lar kullanabilirken kendisi kullanaılaz. 
+    public interface IEntityRepository<T> where T: class,IEntity,new()
     {
         //ürünleri kategoriye göre listele, ürünleri fiyata göre listele gibi filtreler yazabilmemizi sağlar.
         //filer=null ifadesi, filtre vermeyedebilirsin anlamına gelir. Filtre vermediği zaman tüm datayı getir der.
---
+++ DataAccess/Concrete/EntityFramework/NorthWindContext.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    // Context : Db tabloları ile proje classlarını bağlamak.
+    // DbContext : Bir entity framework sınıfıdır. Db ile uygulama arasındaki iletişimi yönetir. NorthWindContext sınıfı DbContext'ten türetilmiştir.
+    public class NorthWindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //hangi tabanına ve nasıl bağlanılacağı bulunur.
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        //Product nesnemi veritabanındaki Products ile bağla demek.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
